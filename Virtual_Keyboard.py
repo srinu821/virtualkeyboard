06db52de7f06343eafcaf9906b3db5c251188a50
srinu821 <83221754+srinu821@users.noreply.github.com>
2021-10-08 17:53:26 +0530

    Add files via upload

---
+++ Virtual_Keyboard.py
@@ -0,0 +1,78 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+import numpy as np
+import imutils
+import cvzone
+from pynput.keyboard import Controller
+
+
+cap = cv2.VideoCapture(0)
+
+detector = HandDetector(detectionCon=0.8)
+
+keys = [["Q","W","E","R","T","Y","U","I","O","P"],
+        ["A","S","D","F","G","H","J","K","L",";"],
+        ["Z","X","C","V","B","N","M",",",".","/"],
+        ["1","2","3","4","5","6","7","8","9","0"]]
+
+finalText = ""
+
+keyboard = Controller()
+
+def draw(img,buttonList):
+    for button in buttonList:
+        x,y = button.pos
+        w,h = button.size
+        cvzone.cornerRect(img,(button.pos[0],button.pos[1],button.size[0],button.size[1]))
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),cv2.FILLED)
+        cv2.putText(img,button.text,(x+14,y+45),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),3)
+    return img
+    
+class Button():
+    def __init__(self,pos,text,size=[60,60]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+    
+buttonList = []
+
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([90*j+50,100*i+50],key))
+
+while True:
+    suc,img = cap.read()
+    img = imutils.resize(img,width = 1280, height = 720)
+    img = detector.findHands(img)
+    lmList,bboxInfo = detector.findPosition(img)
+    img = draw(img,buttonList)
+    
+    if(lmList):
+        for button in buttonList:
+            x,y = button.pos
+            w,h = button.size
+            
+            if x< lmList[8][0] <x+w and y< lmList[8][1]<y+h:
+                cv2.rectangle(img,(x-5,y-5),(x+w+5,y+h+5),(175,0,175),cv2.FILLED)
+                cv2.putText(img,button.text,(x+14,y+45),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),3)
+                
+                l,_,_ = detector.findDistance(8,12,img,draw=False)
+                
+                if l<40:
+                    keyboard.press(button.text)
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),cv2.FILLED)
+                    cv2.putText(img,button.text,(x+14,y+45),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),3)
+                    finalText += button.text
+                    sleep(0.15)
+                    
+    cv2.rectangle(img,(50,450),(930,550),(175,0,175),cv2.FILLED)
+    cv2.putText(img,finalText,(60,510),cv2.FONT_HERSHEY_PLAIN,2,(255,255,255),3)
+                
+    
+    cv2.imshow("image",img)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
